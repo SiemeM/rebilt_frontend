20b51425127b12e8740e1b474ffe11d8927d2a0b
JonasDeBruyne <jonasdebruyne734@gmail.com>
2025-01-06 01:28:06 +0100

    refactor

--- src/main.js
+++ src/main.js
@@ -4,16 +4,4 @@ import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 
-// Import FontAwesome
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-// Add FontAwesome icons to the library
-library.add(faCaretRight);
-
-createApp(App)
-  .component("font-awesome-icon", FontAwesomeIcon) // Register FontAwesome component globally
-  .use(router)
-  .use(createPinia())
-  .mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
