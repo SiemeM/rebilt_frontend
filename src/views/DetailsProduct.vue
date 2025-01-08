835085017a50caae26dc442e0304a129a24df3fc
JonasDeBruyne <jonasdebruyne734@gmail.com>
2025-01-08 08:35:55 +0100

    style: responsive design

--- src/views/DetailsProduct.vue
+++ src/views/DetailsProduct.vue
@@ -20,6 +20,7 @@ const route = useRoute();
 const router = useRouter();
 const productId = ref(null);
 const productData = ref({ productName: "", productCode: "", productPrice: 0 });
+const productName = ref("");
 const isLoading = ref(true);
 const error = ref(null);
 const logoUrl = ref("");
@@ -477,6 +478,7 @@ async function fetchProductData(productCode) {
 
     const result = await response.json();
     const product = result.data.product;
+    productName.value = result.data.product.productName;
 
     // Haal en verrijk configuraties voor dit product
     const configurations = product.configurations || []; // Default to an empty array if configurations is missing
@@ -861,7 +863,7 @@ function showNextImage() {
     </div>
 
     <div class="model" v-if="partnerPackage === 'pro'"></div>
-    <div class="icons">
+    <div class="icons desktop">
       <router-link :to="`/`">
         <div class="icon">
           <div>
@@ -893,7 +895,7 @@ function showNextImage() {
         <p>AR</p>
       </div>
     </div>
-    <div class="rotate-informer" v-if="partnerPackage === 'pro'">
+    <div class="rotate-informer desktop" v-if="partnerPackage === 'pro'">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
@@ -906,6 +908,66 @@ function showNextImage() {
         ></path>
       </svg>
       <p>Drag to rotate</p>
+    </div>
+
+    <div class="mobileElements">
+      <router-link :to="`/`" class="back">
+        <i class="fa fa-angle-left"></i>
+        <p>Back to collections</p>
+      </router-link>
+      <div class="row">
+        <p class="name">{{ productName }}</p>
+        <div class="icons">
+          <div class="icon" v-if="partnerPackage === 'pro'">
+            <div @click="generateQRCode" class="AR">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path
+                  d="M11.98 2.36a.75.75 0 00-.33.1L8.3 4.11H8.3a.08.08 0 00-.01.01.76.76 0 00-.31 1c.09.18.24.31.43.38.19.06.4.05.57-.04l2.26-1.13v2.7c0 .2.08.39.22.53a.76.76 0 001.08 0 .76.76 0 00.22-.54V4.34L15 5.47h.01a.76.76 0 001.03-.91.75.75 0 00-.34-.42l-.01-.01a.08.08 0 00-.01 0l-3.34-1.68a.75.75 0 00-.37-.09zm-5.57 2.8a.76.76 0 00-.34.08L2.72 6.9a.76.76 0 00-.42.7v3.9c0 .2.08.39.22.53a.76.76 0 001.07 0 .76.76 0 00.22-.54V8.81l2.26 1.13a.76.76 0 001.05-.91.76.76 0 00-.35-.42.08.08 0 00-.02-.02l-2-1 2-1c.3-.16.48-.52.4-.85a.77.77 0 00-.74-.58zm11.18 0a.77.77 0 00-.73.58c-.08.32.08.67.37.83l.02.02 2 1-2 1a.08.08 0 00-.02.02c-.17.1-.3.24-.35.42a.76.76 0 00.47.95c.19.06.4.05.58-.04l2.26-1.13v2.7c0 .2.08.39.22.53a.76.76 0 001.07 0 .76.76 0 00.22-.54V7.6a.76.76 0 00-.42-.69l-3.35-1.67a.76.76 0 00-.34-.08zm-2.24 4.47a.75.75 0 00-.33.08L12 11.2l-3.02-1.5a.76.76 0 00-1.05.91c.06.18.2.33.36.42a.08.08 0 000 .01h.01a.08.08 0 000 .01l2.94 1.47v2.89c0 .2.08.39.22.53a.76.76 0 001.08 0 .76.76 0 00.22-.53v-2.9l2.93-1.46.02-.02c.3-.16.45-.5.37-.83a.77.77 0 00-.73-.58zM3.01 14.11a.75.75 0 00-.7.75v3.9c-.01.29.15.56.41.69l3.35 1.67a.76.76 0 001.05-.91.76.76 0 00-.34-.42.08.08 0 00-.03-.02l-2-1 2-1 .02-.02c.17-.1.3-.24.35-.42a.76.76 0 00-.47-.95.77.77 0 00-.58.04l-2.26 1.13v-2.7c0-.2-.08-.4-.24-.55a.76.76 0 00-.56-.2zm17.88 0a.76.76 0 00-.7.75v2.69l-2.26-1.13a.76.76 0 00-1.05.91c.06.18.18.33.35.42a.08.08 0 00.01 0v.02a.08.08 0 00.01 0l2 1-2 1h-.01a.08.08 0 000 .01h-.01a.76.76 0 00.13 1.37c.18.07.39.06.57-.03l3.35-1.67c.26-.13.42-.4.42-.69v-3.9a.76.76 0 00-.8-.75zm-8.94 4.47a.08.08 0 00-.03 0 .75.75 0 00-.47.23.76.76 0 00-.2.52v2.69l-2.26-1.13a.08.08 0 00-.01 0 .76.76 0 00-1.03.91c.05.18.18.33.34.42l.01.01a.08.08 0 00.01.01l3.34 1.67c.22.12.49.11.7 0l3.34-1.67.02-.01a.76.76 0 00.31-1 .76.76 0 00-1-.34l-2.26 1.13v-2.7a.76.76 0 00-.8-.74z"
+                ></path>
+              </svg>
+            </div>
+            <p>AR</p>
+          </div>
+        </div>
+      </div>
+      <div class="bigImageWithImages">
+        <div
+          class="bigImage"
+          style="
+            background-image: url('https://res.cloudinary.com/dzempjvto/image/upload/v1736262373/Configurations/Kleur/Blauw/wgog3irgrnqnkseh9ubg.jpg');
+          "
+        ></div>
+        <div class="images">
+          <div
+            class="image active"
+            style="
+              background-image: url('https://res.cloudinary.com/dzempjvto/image/upload/v1736262373/Configurations/Kleur/Blauw/wgog3irgrnqnkseh9ubg.jpg');
+            "
+          ></div>
+          <div
+            class="image"
+            style="
+              background-image: url('https://res.cloudinary.com/dzempjvto/image/upload/v1736262373/Configurations/Kleur/Blauw/nl9ll2prnglj9k8iwtll.jpg');
+            "
+          ></div>
+        </div>
+      </div>
+      <div class="model" v-if="partnerPackage === 'pro'"></div>
+
+      <div class="rotate-informer desktop" v-if="partnerPackage === 'pro'">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="#ffffff"
+          viewBox="0 0 256 256"
+        >
+          <path
+            d="M232,48V88a8,8,0,0,1-16,0V56H184a8,8,0,0,1,0-16h40A8,8,0,0,1,232,48ZM72,200H40V168a8,8,0,0,0-16,0v40a8,8,0,0,0,8,8H72a8,8,0,0,0,0-16Zm152-40a8,8,0,0,0-8,8v32H184a8,8,0,0,0,0,16h40a8,8,0,0,0,8-8V168A8,8,0,0,0,224,160ZM32,96a8,8,0,0,0,8-8V56H72a8,8,0,0,0,0-16H32a8,8,0,0,0-8,8V88A8,8,0,0,0,32,96ZM188,167l-56,32a8,8,0,0,1-7.94,0L68,167A8,8,0,0,1,64,160V96a8,8,0,0,1,4-7l56-32a8,8,0,0,1,7.94,0l56,32a8,8,0,0,1,4,7v64A8,8,0,0,1,188,167ZM88.12,96,128,118.79,167.88,96,128,73.21ZM80,155.36l40,22.85V132.64L80,109.79Zm96,0V109.79l-40,22.85v45.57Z"
+          ></path>
+        </svg>
+        <p>Drag to rotate</p>
+      </div>
     </div>
     <div class="config-wrapper">
       <div class="overview">
