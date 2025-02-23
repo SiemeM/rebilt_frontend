288bec180aea6abaebe08aef8036a6135d622483
SiemeM <147379958+SiemeM@users.noreply.github.com>
2025-02-23 14:16:26 +0100

    Update: FaceTracking

--- src/components/FaceTracking.vue
+++ src/components/FaceTracking.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="ar-container">
     <video ref="videoElement" autoplay playsinline></video>
-    <canvas ref="canvasElement"></canvas>
+    <canvas ref="canvasElement" width="640" height="480"></canvas>
   </div>
 </template>
 
 <script>
+/**
+ * LET OP:
+ *  - We importeren expliciet face_mesh, camera_utils en drawing_utils.
+ *  - We overschrijven 'locateFile' zodat bij het laden van de WASM niet de simd-bestanden
+ *    worden gebruikt, maar de NON-SIMD versie.
+ */
 import { FaceMesh } from "@mediapipe/face_mesh";
 import { Camera } from "@mediapipe/camera_utils";
 import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
 import { FACEMESH_TESSELATION } from "@mediapipe/face_mesh";
 
 export default {
+  name: 'FaceTracking',
+
   data() {
     return {
       video: null,
@@ -22,21 +30,19 @@ export default {
   },
 
   mounted() {
-    this.init();
+    this.initCamera();
   },
 
   methods: {
-    async init() {
-      this.video = this.$refs.videoElement;
-      this.canvas = this.$refs.canvasElement;
-      this.ctx = this.canvas.getContext("2d");
-
-      // Start de camera
+    async initCamera() {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({
           video: { facingMode: "user" },
         });
+        this.video = this.$refs.videoElement;
         this.video.srcObject = stream;
+
+        // Als de metadata geladen is (video breedte/hoogte etc.), start FaceMesh-initialisatie
         this.video.onloadedmetadata = () => {
           this.video.play();
           this.initFaceMesh();
@@ -46,15 +52,22 @@ export default {
       }
     },
 
-    async initFaceMesh() {
-      // Initialiseer FaceMesh
+    initFaceMesh() {
+      // We gaan expliciet de wasm-bestanden herdefiniëren, zodat we de NON-SIMD versie pakken
+      // in plaats van face_mesh_solution_simd_wasm_bin.js.
+      // Let op versie: gebruik bij voorkeur de meest recente @mediapipe/face_mesh-versie.
       this.faceMesh = new FaceMesh({
         locateFile: (file) => {
-          return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+          // Controle: als de lib de simd-versie zoekt, vervangen we het door de non-simd wasm:
+          if (file === 'face_mesh_solution_simd_wasm_bin.js') {
+            return 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh@0.4/face_mesh_solution_wasm_bin.js';
+          }
+          // Anders laat je het gewoon naar hetzelfde pad verwijzen:
+          return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh@0.4/${file}`;
         },
       });
 
-      // Stel opties in voor FaceMesh
+      // Stel FaceMesh-opties in (eventueel tweaken naar wens)
       this.faceMesh.setOptions({
         maxNumFaces: 1,
         refineLandmarks: true,
@@ -62,24 +75,32 @@ export default {
         minTrackingConfidence: 0.7,
       });
 
-      // Verwerk de resultaten
+      // Koppel je onResults-callback
       this.faceMesh.onResults(this.onFaceMeshResults);
 
-      // Start de camera en FaceMesh
-      new Camera(this.video, {
+      // Start de Mediapipe Camera helper
+      const camera = new Camera(this.$refs.videoElement, {
         onFrame: async () => {
-          await this.faceMesh.send({ image: this.video });
+          await this.faceMesh.send({ image: this.$refs.videoElement });
         },
         width: 640,
         height: 480,
-      }).start();
+      });
+      camera.start();
     },
 
     onFaceMeshResults(results) {
-      // Teken de resultaten op het canvas
+      // Context pakken
+      if (!this.canvas) this.canvas = this.$refs.canvasElement;
+      if (!this.ctx) this.ctx = this.canvas.getContext("2d");
+
+      // Canvas legen
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      // De (door FaceMesh bewerkte) image terugzetten
       this.ctx.drawImage(results.image, 0, 0, this.canvas.width, this.canvas.height);
 
+      // Teken de gezichtslandmarks (optioneel)
       if (results.multiFaceLandmarks) {
         for (const landmarks of results.multiFaceLandmarks) {
           drawConnectors(this.ctx, landmarks, FACEMESH_TESSELATION, {
@@ -94,7 +115,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .ar-container {
   position: fixed;
   top: 0;
@@ -102,13 +123,15 @@ export default {
   width: 100vw;
   height: 100vh;
   overflow: hidden;
+  /* Zet eventueel een zwarte achtergrond om te debuggen */
+  background: #000;
 }
 
 video {
   width: 100%;
   height: 100%;
   position: absolute;
-  transform: scaleX(-1);
+  transform: scaleX(-1); /* Front-facing camera lijkt hierdoor realistischer */
   object-fit: cover;
 }
 
