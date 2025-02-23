8bd09c8dad4f86ffe0a5cd59f506265449b3975e
SiemeM <147379958+SiemeM@users.noreply.github.com>
2025-02-23 14:47:10 +0100

    Update: FaceTracking VITE

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: {
+    exclude: ["@mediapipe/face_mesh"]
+  },
   server: {
     proxy: {
       "/api": {
