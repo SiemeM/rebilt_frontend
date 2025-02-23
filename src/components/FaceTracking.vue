f3e4e330bcd2feb01c660316acbc96ad389ab832
SiemeM <147379958+SiemeM@users.noreply.github.com>
2025-02-23 14:29:19 +0100

    Update: FaceTracking

--- src/components/FaceTracking.vue
+++ src/components/FaceTracking.vue
@@ -12,10 +12,6 @@ import { Camera } from "@mediapipe/camera_utils";
 import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
 import { FACEMESH_TESSELATION } from "@mediapipe/face_mesh";
 
-/**
- * Eventuele console.debug(...) of console.log(...) staan uitgebreid
- * in de code om precies te kunnen volgen waar iets eventueel misgaat.
- */
 export default {
   name: "FaceTracking",
   setup() {
@@ -37,7 +33,6 @@ export default {
         });
         console.log("[FaceTracking] Camera stream granted.");
 
-        // Koppel stream aan video
         videoElement.value.srcObject = stream;
         videoElement.value.onloadedmetadata = () => {
           console.log("[FaceTracking] Video metadata loaded, playing video & init FaceMesh...");
@@ -53,28 +48,9 @@ export default {
       console.log("[FaceTracking] initFaceMesh() called.");
 
       const faceMesh = new FaceMesh({
-        /**
-         * We overschrijven locateFile om de NON-SIMD wasm te laden (face_mesh_solution_wasm_bin.*)
-         * i.p.v. face_mesh_solution_simd_wasm_bin.js.
-         *
-         * Let op: De bestandsnamen in @mediapipe/face_mesh@0.8.1 zijn:
-         *   - face_mesh_solution_packed_assets_loader.js
-         *   - face_mesh_solution_wasm_bin.js
-         *   - face_mesh_solution_wasm_bin.wasm
-         */
         locateFile: (file) => {
           console.debug("[FaceTracking] FaceMesh is requesting file:", file);
-
-          // Als er om de SIMD versie gevraagd wordt:
-          if (file === "face_mesh_solution_simd_wasm_bin.js") {
-            console.warn("[FaceTracking] Replacing SIMD WASM with NON-SIMD => face_mesh_solution_wasm_bin.js");
-            return "/node_modules/@mediapipe/face_mesh/face_mesh_solution_wasm_bin.js";
-          }
-
-          // Anders: Laat alle andere files naar de non-SIMD folder wijzen.
-          // (Je kunt deze paths aanpassen of dynamisch genereren; het gaat erom dat
-          //  je in je bundel of je dev-server de 'node_modules' folder serveert.)
-          return `/node_modules/@mediapipe/face_mesh/${file}`;
+          return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
         },
       });
 
@@ -92,7 +68,6 @@ export default {
       console.log("[FaceTracking] Creating Camera instance...");
       const camera = new Camera(videoElement.value, {
         onFrame: async () => {
-          // Stuur elke frame door naar FaceMesh
           console.debug("[FaceTracking] onFrame -> sending frame to FaceMesh...");
           await faceMesh.send({ image: videoElement.value });
         },
@@ -112,20 +87,13 @@ export default {
         ctx = canvasElement.value.getContext("2d");
       }
 
-      // Canvas schoonmaken
       ctx.clearRect(0, 0, canvasElement.value.width, canvasElement.value.height);
-
-      // Teken de 'image' op het canvas
       ctx.drawImage(results.image, 0, 0, canvasElement.value.width, canvasElement.value.height);
 
-      // Als er landmarks zijn, tekenen we ze
       if (results.multiFaceLandmarks && results.multiFaceLandmarks.length > 0) {
         console.log(`[FaceTracking] Detected ${results.multiFaceLandmarks.length} face(s). Drawing landmarks...`);
         for (const landmarks of results.multiFaceLandmarks) {
-          drawConnectors(ctx, landmarks, FACEMESH_TESSELATION, {
-            color: "#00FF00",
-            lineWidth: 1,
-          });
+          drawConnectors(ctx, landmarks, FACEMESH_TESSELATION, { color: "#00FF00", lineWidth: 1 });
           drawLandmarks(ctx, landmarks, { color: "#FF0000", radius: 2 });
         }
       } else {
@@ -156,7 +124,7 @@ video {
   width: 100%;
   height: 100%;
   position: absolute;
-  transform: scaleX(-1); /* Gespiegeld voor front-facing camera */
+  transform: scaleX(-1);
   object-fit: cover;
 }
 
